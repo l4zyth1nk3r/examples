1da1a1f4608ba1cab1fc989e52674860d8187f2b
l4zyth1nk3r <jorkut.trading@gmail.com>
2021-04-02 20:40:04 +0200

    add python project structure

---
+++ python_project_structure/tests/context.py
@@ -0,0 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import module
+
+# insert in the test files this import -> "from .context import sample"
